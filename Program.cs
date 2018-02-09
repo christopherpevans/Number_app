d9656a59953fba21e9b97306bedeb2b9dd460d7f
Princetonevans <princetonevans@yahoo.com>
2018-02-09 15:06:23 -0600

    Loop to get users input, error message and color reset.

--- Program.cs
+++ Program.cs
@@ -29,6 +29,38 @@ namespace Number_app
             string inputName = Console.ReadLine();
 
             Console.WriteLine("Hello {0}, let's play a game...", inputName);
+
+            // Set correct number
+            int correctNumber = 7;
+
+            // Set guess var
+            int guess = 0;
+
+            // Ask user for number
+            Console.WriteLine("Guess a number between 1 and 10");
+
+            // While guess is not correct
+            while(guess != correctNumber) {
+                // Get users input
+                string input = Console.ReadLine();
+
+                // Cast to int and put in guess
+                guess = Int32.Parse(input);
+
+                // Match guess to correct number
+                if (guess != correctNumber) 
+                {
+                    // Change text color
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+
+                    // Write app info
+                    Console.WriteLine("Wrong number, please try again");
+
+                    //Reset text color
+                    Console.ResetColor();
+                }
+            }
         }
     }
 }
