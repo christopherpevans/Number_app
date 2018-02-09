bc7a038728082a087b616cc8c5faa4722a0e7523
Princetonevans <princetonevans@yahoo.com>
2018-02-09 14:45:51 -0600

    Set app variables

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Number_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Set app vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Princeton Evans";
+
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+        }
+    }
+}
