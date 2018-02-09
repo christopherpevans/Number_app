12f181a01b66b9d1d771fbea17a10ccece009186
Princetonevans <princetonevans@yahoo.com>
2018-02-09 14:47:06 -0600

    Changed text color to green

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ namespace Number_app
             string appVersion = "1.0.0";
             string appAuthor = "Princeton Evans";
 
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.Green;
+
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
         }
     }
