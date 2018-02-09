cb2124a238303a0d416f05d210e689f3c5673b76
Princetonevans <princetonevans@yahoo.com>
2018-02-09 14:50:39 -0600

    Added user input

--- Program.cs
+++ Program.cs
@@ -19,7 +19,16 @@ namespace Number_app
             // Write app info
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
 
+            //Reset text color
             Console.ResetColor();
+
+            // Ask users name
+            Console.WriteLine("What is your name?");
+
+            // Get user input
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, let's play a game...", inputName);
         }
     }
 }
